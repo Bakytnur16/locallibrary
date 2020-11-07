cad87942903233793f752d08ba3165fffa69c1cb
Bakytnur16 <suakbakytnur@gmail.com>
2020-11-07 14:48:07 +0600

    0111

--- catalog/views.py
+++ catalog/views.py
@@ -15,9 +15,37 @@ def index(request):
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # The 'all()' is implied by default.
     
-    # Render the HTML template index.html with the data in the context variable
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+    
+    # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,
+            'num_visits':num_visits}, # num_visits appended
     )
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+
+def book_detail_view(request,pk):
+    try:
+        book_id=Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+
+    #book_id=get_object_or_404(Book, pk=pk)
+    
+    return render(
+        request,
+        'catalog/book_detail.html',
+        context={'book':book_id,}
+    )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
